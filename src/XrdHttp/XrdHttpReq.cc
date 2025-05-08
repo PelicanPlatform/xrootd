4057322a5fa7ec2070d6b5e6cf30d3c726d7ba04
Emma Turetsky <turetske@gmail.com>
2025-05-08 10:51:11 -0500

    [#25] XRootD's xml response for PROPFIND will now include the resourcetype tag for non-collections

--- src/XrdHttp/XrdHttpReq.cc
+++ src/XrdHttp/XrdHttpReq.cc
@@ -2445,6 +2445,7 @@ int XrdHttpReq::PostProcessHTTPReq(bool final_) {
                 stringresp += "<lp1:resourcetype><D:collection/></lp1:resourcetype>\n";
                 stringresp += "<lp1:iscollection>1</lp1:iscollection>\n";
               } else {
+                stringresp += "<lp1:resourcetype/>\n";
                 stringresp += "<lp1:iscollection>0</lp1:iscollection>\n";
               }
 
@@ -2561,6 +2562,7 @@ int XrdHttpReq::PostProcessHTTPReq(bool final_) {
                   stringresp += "<lp1:resourcetype><D:collection/></lp1:resourcetype>\n";
                   stringresp += "<lp1:iscollection>1</lp1:iscollection>\n";
                 } else {
+                  stringresp += "<lp1:resourcetype/>\n";
                   stringresp += "<lp1:iscollection>0</lp1:iscollection>\n";
                 }
 
